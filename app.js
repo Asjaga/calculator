b94998e311d3c22a8467ef1234c34a67e46740e3
Aakash Singh <Aakashsinghjaga@gmail.com>
2025-05-23 11:10:49 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,47 +1,58 @@
 let boxes = document.querySelectorAll(".box");
-let msg =  document.querySelector(".msg-screen");
-let msg2 =  document.querySelector(".msg-screen2");
-msg2.innerText = "";
+let msg = document.querySelector(".msg-screen");
+let msg2 = document.querySelector(".msg-screen2");
+let expression = "";
+let bs = document.querySelector(".bs");
 
-let expression ="";
-
-
-function shortNum(num) {
-    if (Number.isInteger(num)) {
+let shortNum = (num)=>{
+    if (Number.isInteger(num)){
         return num;
-    } else {
+    }else{
         return parseFloat(num.toFixed(7));
     }
 }
+for(let box of boxes){
+    box.addEventListener("click",()=>{
+        msg.style.fontSize = "3.5rem";
+        msg2.style.fontSize = "3.5rem";
 
-boxes.forEach((box) =>{
-    box.addEventListener("click", ()=>{
-        if (box.id == "="){
+        if (box.id === "="){
             try{
-                msg2.innerText = shortNum(eval(expression));
-                expression = eval(expression);
+                msg.innerText = shortNum(eval(expression));
             }
-            catch(err){
-                msg2.innerText = "Error";
+            catch {
+                msg.innerText = "Error";
+            }            
+        }
+        else if (box.id === "%"){
+            msg.innerText = msg.innerText/100;
+        }
+        else if (box.id === "bs"){
+            expression = expression.slice(0,-1);
+            msg.innerText = msg.innerText.slice(0,-1);
+            if (expression === ""){
+                    msg.classList.add("dis");
+                    msg2.classList.remove("dis");
             }
         }
-        else if (box.id == "c"){
-            msg2.innerText = "";
-            msg2.classList.add("dis");
-            msg.classList.remove("dis");           
+        else if (box.id === "c"){
+            msg.innerText = "";
             expression = "";
+            msg.classList.add("dis");
+            msg2.classList.remove("dis");
         }
-        else if (box.id ==="%"){
-            msg2.innerText = expression/100;
-            expression = expression/100;
-        }
-        else { 
-                msg2.classList.remove("dis");
-                msg.classList.add("dis");
-                expression = expression + box.id;
-                msg2.innerText = msg2.innerText + box.innerText;
-
+        else{
+            if (msg.innerText.length<10){
+                 msg.classList.remove("dis");
+                 msg2.classList.add("dis");
+                 msg.innerText = msg.innerText + box.innerText ;
+                 expression = expression + box.id;
+            }
+            else{
+                msg.style.fontSize = "2.5rem";
+                msg.innerText = "Not Allowed"
+            }
+           
         }
     })
-})
-
+}
